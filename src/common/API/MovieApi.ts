97067c55e4518f701300e2eee0486e3cd1b898c6
glunfront <glunfront@gmail.com>
2022-06-08 22:59:16 +0500

    updFetch function

--- src/common/API/MovieApi.ts
+++ src/common/API/MovieApi.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
+import {apiKey} from './key/apiKeyMovie'; 
 
-export default axios.create({
+const fetchUrl = axios.create({
     baseURL: 'http://img.omdbapi.com'
 })
+
+const movieText = 'Harry'
+
+export const fetchMovies = async () => {
+    try {
+        const response = await fetchUrl.get(`?apikey=${apiKey}&s=${movieText}&type=movie`)
+        console.log(response.data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,20 +2,12 @@ import React, { useEffect, useState } from 'react';
 import MovieListing from '../../components/MovieListing/MovieListing';
 import axios from 'axios';
 import { apiKey } from '../../common/API/key/apiKeyMovie';
-import movieApi from '../../common/API/MovieApi';
+import { fetchMovies } from '../../common/API/MovieApi';
 
 const Home = () => {
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
-        const movieText = 'Harry'
-        const fetchMovies = async () => {
-            const response = await movieApi.get(`?apikey=${apiKey}&s=${movieText}&type=movie`)
-            .catch((err) => {
-                console.log(err)
-            })
-            console.log(response)
-        }
         fetchMovies()
     }, [])
 
