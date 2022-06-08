962260811e227b22aa588ac492a75077678c975e
glunfront <glunfront@gmail.com>
2022-06-08 22:49:00 +0500

    Home-component_upd

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MovieListing from '../../components/MovieListing/MovieListing';
+import axios from 'axios';
+import movieApi from '../../common/API/MovieApi';
+import { apiKey } from '../../common/API/apiKeyMovie';
 
 const Home = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const movieText = 'Harry'
+        const fetchMovies = async () => {
+            const response = await movieApi.get(`?apikey=${apiKey}&s=${movieText}&type=movie`)
+            .catch((err) => {
+                console.log(err)
+            })
+            console.log(response)
+        }
+        fetchMovies()
+    }, [])
+
     return (
         <div>
-            Home
+            <div className='banner-img'></div>
+            <MovieListing />
         </div>
     );
 };
